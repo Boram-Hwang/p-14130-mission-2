f44bf704e71f25235ed0bfbd885b7c0b57304683
boramH <ky26212621@gmail.com>
2026-07-15 16:46:45 +0900

    4단계 등록할때마다 생성되는 명언번호가 증가

--- src/main/kotlin/com/Main.kt
+++ src/main/kotlin/com/Main.kt
@@ -1,8 +1,15 @@
 package com
 
+data class WiseSaying(
+    val id: Int,
+    val content: String?,
+    val author: String?
+)
+
 fun main() {
     println("== 명언 앱 ==")
     var index = 0;
+    val wiseSayings = mutableListOf<WiseSaying>()
 
     while (true) {
         print("명령) ")
@@ -17,7 +24,15 @@ fun main() {
             print("작가 : ")
             val author = readlnOrNull()?.trim()
 
-            println("${++index}번 명언이 등록되었습니다.")
+            val wiseSaying = WiseSaying(
+                id = ++index,
+                content = content,
+                author = author
+            )
+
+            wiseSayings.add(wiseSaying)
+
+            println("${wiseSaying.id}번 명언이 등록되었습니다.")
         }
     }
 }
